6428d7e8597f8c108da672073872f8cfa144f196
siddhartha gupta <sgupta2@my.westga.edu>
2019-01-22 00:16:41 -0600

    Designed the incident model class and the DAL class for incidents

---
+++ TechSupport/DAL/IncidentDAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechSupport.Model;
+
+namespace TechSupport.DAL
+{
+    public class IncidentDAL
+    {
+        private static List<Incident> _customerIncident = new List<Incident>
+        {
+            new Incident("Bathroom maintainence","plumbing is broken",101),
+            new Incident("Hallway maintainence","Table is broken",101),
+        };
+
+    }
+}
---
+++ TechSupport/Model/Incident.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechSupport.Model
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Incident
+    {
+        public string Title { get; }
+        public string Description { get; }
+        public int CustomerID { get; }
+
+
+
+        public Incident(string title, string description, int customerID)
+        {
+            if (string.IsNullOrEmpty(title))
+            {
+                throw new ArgumentException("Incident title can not be null or empty", "title");
+            }
+
+            if (string.IsNullOrEmpty(description))
+            {
+                throw new ArgumentException("Incident description can not be null or empty", "description");
+            }
+
+            if (customerID < 1 )
+            {
+                throw new ArgumentException("CustomerID needs to be greater than 0", "customerID");
+            }
+
+            this.CustomerID = customerID;
+            this.Description = description;
+            this.Title = title;
+        }
+
+        
+
+
+    }
+}
