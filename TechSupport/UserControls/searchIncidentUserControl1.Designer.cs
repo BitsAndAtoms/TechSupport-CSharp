471d60023bc3c8a21e5f41ad820d13d8961ca677
siddhartha gupta <sgupta2@my.westga.edu>
2019-02-04 21:52:38 -0600

    created search incident user control. Will add it next. Commit before possible break.

---
+++ TechSupport/UserControls/searchIncidentUserControl1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace TechSupport.UserControls
+{
+    partial class searchIncidentUserControl1
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.searchCustomerIDField = new System.Windows.Forms.NumericUpDown();
+            this.Cancel = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.searchDataGridView = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.searchCustomerIDField)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.searchDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.searchButton, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.Cancel, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.searchCustomerIDField, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(390, 100);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Customer ID";
+            // 
+            // searchCustomerIDField
+            // 
+            this.searchCustomerIDField.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.searchCustomerIDField.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchCustomerIDField.Location = new System.Drawing.Point(232, 10);
+            this.searchCustomerIDField.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.searchCustomerIDField.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.searchCustomerIDField.Name = "searchCustomerIDField";
+            this.searchCustomerIDField.Size = new System.Drawing.Size(120, 29);
+            this.searchCustomerIDField.TabIndex = 5;
+            this.searchCustomerIDField.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.searchCustomerIDField.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Cancel
+            // 
+            this.Cancel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Cancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cancel.Location = new System.Drawing.Point(53, 55);
+            this.Cancel.Name = "Cancel";
+            this.Cancel.Size = new System.Drawing.Size(88, 40);
+            this.Cancel.TabIndex = 4;
+            this.Cancel.Text = "Reset";
+            this.Cancel.UseVisualStyleBackColor = true;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.searchButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchButton.Location = new System.Drawing.Point(240, 55);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(104, 40);
+            this.searchButton.TabIndex = 6;
+            this.searchButton.Text = "Search Button";
+            this.searchButton.UseVisualStyleBackColor = true;
+            // 
+            // searchDataGridView
+            // 
+            this.searchDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.searchDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.searchDataGridView.Location = new System.Drawing.Point(0, 100);
+            this.searchDataGridView.Name = "searchDataGridView";
+            this.searchDataGridView.Size = new System.Drawing.Size(390, 221);
+            this.searchDataGridView.TabIndex = 4;
+            // 
+            // searchIncidentUserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.searchDataGridView);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "searchIncidentUserControl1";
+            this.Size = new System.Drawing.Size(390, 321);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.searchCustomerIDField)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.searchDataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown searchCustomerIDField;
+        private System.Windows.Forms.Button Cancel;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.DataGridView searchDataGridView;
+    }
+}
