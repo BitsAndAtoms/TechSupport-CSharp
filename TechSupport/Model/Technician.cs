c5c11e67e944d8ea065bae9194799ad3c57c1649
Sid Gupta <sgupta2@my.westga.edu>
2019-03-11 23:26:25 -0500

    Technician class created

---
+++ TechSupport/Model/Technician.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace TechSupport.Model
+{
+    class Technician
+    {
+            /// <summary>
+            /// email of the technician get set
+            /// </summary>
+            public string Email { get; set; }
+
+            /// <summary>
+            /// Phone of the technician get set
+            /// </summary>
+            public string Phone { get; set; }
+
+        
+            /// <summary>
+            /// Name of the technician get set
+            /// </summary>
+            public string TechnicianName { get; set; }
+
+        
+    }
+}
