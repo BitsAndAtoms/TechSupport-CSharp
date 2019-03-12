390c7874c349fcbbff8864c22e7c3c0ff1cff3cc
Sid Gupta <sgupta2@my.westga.edu>
2019-03-12 01:00:01 -0500

    Tied the form 1 combo box to technician name.

--- TechSupport/View/Form1.cs
+++ TechSupport/View/Form1.cs
@@ -7,14 +7,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TechSupport.Controller;
+using TechSupport.Model;
 
 namespace TechSupport.View
 {
+    
     public partial class Form1 : Form
     {
+        private TechnicianController newTechnicianController;
         public Form1()
         {
             InitializeComponent();
+            newTechnicianController = new TechnicianController();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<Technician> technicianList = newTechnicianController.GETTechnicianListFromDBWithOpenIncidents();
+            technicianNameComboBox.DataSource = technicianList;
+
+
+
         }
     }
 }
