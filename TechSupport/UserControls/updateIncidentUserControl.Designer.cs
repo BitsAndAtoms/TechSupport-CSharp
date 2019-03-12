a98bdf688f52c1a4da12ab768f1b26dd08a714ec
Sid Gupta <sgupta2@my.westga.edu>
2019-03-12 02:52:55 -0500

    HW5 issue 1: Scrollbar added to description area

--- TechSupport/UserControls/updateIncidentUserControl.Designer.cs
+++ TechSupport/UserControls/updateIncidentUserControl.Designer.cs
@@ -137,7 +137,7 @@
             this.tableLayoutPanel2.ColumnCount = 3;
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 46F));
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 54F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 105F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 106F));
             this.tableLayoutPanel2.Controls.Add(this.clearButton, 0, 0);
             this.tableLayoutPanel2.Controls.Add(this.closeButton, 0, 0);
             this.tableLayoutPanel2.Controls.Add(this.updateButton, 0, 0);
@@ -153,7 +153,7 @@
             // 
             this.clearButton.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.clearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.clearButton.Location = new System.Drawing.Point(337, 48);
+            this.clearButton.Location = new System.Drawing.Point(336, 48);
             this.clearButton.Name = "clearButton";
             this.clearButton.Size = new System.Drawing.Size(68, 29);
             this.clearButton.TabIndex = 11;
@@ -166,7 +166,7 @@
             this.closeButton.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.closeButton.Enabled = false;
             this.closeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.closeButton.Location = new System.Drawing.Point(157, 48);
+            this.closeButton.Location = new System.Drawing.Point(156, 48);
             this.closeButton.Name = "closeButton";
             this.closeButton.Size = new System.Drawing.Size(89, 29);
             this.closeButton.TabIndex = 10;
@@ -325,6 +325,7 @@
             this.descriptionTextBox.Location = new System.Drawing.Point(140, 163);
             this.descriptionTextBox.Multiline = true;
             this.descriptionTextBox.Name = "descriptionTextBox";
+            this.descriptionTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.descriptionTextBox.Size = new System.Drawing.Size(293, 44);
             this.descriptionTextBox.TabIndex = 7;
             // 
